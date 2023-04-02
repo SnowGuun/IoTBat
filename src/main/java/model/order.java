2e92d5fbbb47ba03d965d7bc6bdb2e3f9e544c35
MartinRad123 <126545272+MartinRad123@users.noreply.github.com>
2023-04-02 20:19:52 +1000

    Update order.java

--- src/main/java/model/order.java
+++ src/main/java/model/order.java
@@ -9,6 +9,42 @@ package model;
  *
  * @author Dev
  */
-public class order {
-    
+public class Order {
+    private String product;
+    private int quantity;
+    private String address;
+    private String paymentMethod;
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
 }
+
